33e5e314cdef53b81facec1a3a7e3a70f58b2449
Bebez <jsumantri41@students.calvin.ac.id>
2022-11-26 12:48:18 +0700

    render

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder='assets')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
